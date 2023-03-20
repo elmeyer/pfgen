b4b016080def4209f19b56ee812e84265b3a7285
Lars Meyer <lars.meyer@enginsight.com>
2023-03-20 10:08:52 +0100

    fix: allow all protocols from /etc/protocols

--- rule_protocol.go
+++ rule_protocol.go
@@ -1,7 +1,11 @@
 package pf
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // #include <net/if.h>
@@ -22,17 +26,54 @@ const (
 	ProtocolICMP Protocol = C.IPPROTO_ICMP
 )
 
+// Default set of protocols.
+var Protocols = map[int]string{
+	int(ProtocolAny):  "any",
+	int(ProtocolICMP): "icmp",
+	int(ProtocolTCP):  "tcp",
+	int(ProtocolUDP):  "udp",
+}
+
+// Attempt to read valid protocols from the system's /etc/protocols file.
+// Adapted from go/src/net/lookup_unix.go
+func init() {
+	file, err := os.Open("/etc/protocols")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		// tcp    6   TCP    # transmission control protocol
+		if i := strings.Index(line, "#"); i >= 0 {
+			line = line[0:i]
+		}
+		var f []string
+		for _, separator := range []string{" ", "\r", "\t", "\n"} {
+			f = strings.Split(line, separator)
+			if len(f) >= 2 {
+				break
+			}
+		}
+		if len(f) < 2 {
+			continue
+		}
+		if proto, err := strconv.Atoi(strings.TrimSpace(f[1])); err == nil {
+			// We ignore all but the first entries.
+			if _, ok := Protocols[proto]; !ok {
+				Protocols[proto] = strings.TrimSpace(f[0])
+			}
+		}
+	}
+}
+
 func (p Protocol) String() string {
-	switch p {
-	case ProtocolAny:
-		return "any"
-	case ProtocolTCP:
-		return "tcp"
-	case ProtocolUDP:
-		return "udp"
-	case ProtocolICMP:
-		return "icmp"
-	default:
+	if s, ok := Protocols[int(p)]; ok {
+		return s
+	} else {
 		return fmt.Sprintf("Protocol(%d)", p)
 	}
 }
